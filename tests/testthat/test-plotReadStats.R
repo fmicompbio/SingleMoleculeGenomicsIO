7dc9f55cf291a2844ce2824afe2ebe039073d728
mbstadler <stadler.michael@gmail.com>
2025-12-01 16:23:46 +0100

    add plotReadStats
    Co-authored-by: Charlotte Soneson <charlottesoneson@gmail.com>
    Co-authored-by: Michael Stadler <stadler.michael@gmail.com>

---
+++ tests/testthat/test-plotReadStats.R
@@ -0,0 +1,33 @@
+test_that("plotReadStats works", {
+    # example data
+    bamf <- system.file("extdata", "6mA_1_10reads.bam",
+                        package = "SingleMoleculeGenomicsIO")
+    se <- readModBam(bamf, regions = "chr1:6940000-6955000", modbase = "a",
+                     BPPARAM = BiocParallel::SerialParam())
+    expect_warning(
+        se <- addReadStats(se, BPPARAM = BiocParallel::SerialParam()),
+        "Too few points")
+
+    setmp <- se
+    names(setmp$QC) <- "wrong_name"
+    expect_error(plotReadStats(setmp),
+                 "names of .se\\$readInfo. and .se\\$QC. are not identical")
+    rm(setmp)
+
+    gg <- plotReadStats(se)
+    expect_true(ggplot2::is_ggplot(gg))
+
+    gg <- plotReadStats(se, readInfoCol = NULL)
+    expect_true(ggplot2::is_ggplot(gg))
+
+    gg <- plotReadStats(se, qcCol = NULL)
+    expect_true(ggplot2::is_ggplot(gg))
+
+    expect_error(plotReadStats(se, qcCol = NULL,readInfoCol = NULL),
+                 "Both are NULL")
+
+    #SE is not normally part of the defaults:
+    seSE <- addReadStats(se,stats=c("SEntrModProb"), BPPARAM = BiocParallel::SerialParam())
+    gg <- plotReadStats(seSE)
+    expect_true(ggplot2::is_ggplot(gg))
+})
